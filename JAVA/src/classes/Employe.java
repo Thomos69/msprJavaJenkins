f53049346e3a118ed1017af62805f6cc95b90555
ThomosLanimos <thomasgaujard@gmail.com>
2022-06-20 21:43:52 +0200

    change hash to sha 256

--- JAVA/src/classes/Employe.java
+++ JAVA/src/classes/Employe.java
@@ -45,17 +45,12 @@ public class Employe {
     }
 
     public String getHashedMotDePasse() throws NoSuchAlgorithmException {
-        // Static getInstance method is called with hashing MD5
-        MessageDigest md = MessageDigest.getInstance("MD5");
 
-        // digest() method is called to calculate message digest
-        //  of an input digest() return array of byte
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
         byte[] messageDigest = md.digest(this.motDePasse.getBytes());
 
-        // Convert byte array into signum representation
         BigInteger no = new BigInteger(1, messageDigest);
 
-        // Convert message digest into hex value
         String hashtext = no.toString(16);
         while (hashtext.length() < 32) {
             hashtext = "0" + hashtext;
